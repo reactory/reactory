ba6d767c19e46b6b4e2f68c5ed38d8b64730b9d0
Richard King <richrdkng@gmail.com>
2023-09-18 11:52:23 +0200

    build(class-name): remove UMD build from configs

--- packages/class-name/rollup.config.js
+++ packages/class-name/rollup.config.js
@@ -1,6 +1,5 @@
 const config = require('../../shared/rollup.config')
-const pkg = require('./package.json')
 
-module.exports = [
-  ...config(pkg, 'reactoryClassName')
-]
+module.exports = {
+  ...config
+}
